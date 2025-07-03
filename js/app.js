86d07c578dad7680e2c18ea81c4f999bafb7cedf
erfan-ne <erfan.neshati@gmail.com>
2025-07-03 19:55:16 +0330

    fit(app.js): read user laptop battery

--- js/app.js
+++ js/app.js
@@ -1,3 +1,12 @@
-navigator.getBattery()
-    .then((resault)=>{console.log(resault);
-    })
+const batteryPercentage = document.querySelector(".battery__percentage");
+const batteryStatus = document.querySelector(".battery__status");
+const batteryLiquid = document.querySelector(".battery__liquid");
+
+const showBatteryInfo = () => {
+  navigator.getBattery().then((resault) => {
+    console.log(resault);
+
+  });
+};
+
+window.addEventListener("load", showBatteryInfo);
