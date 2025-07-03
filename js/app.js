1d3952041222d61744529d04a25987250dec9d7a
erfan-ne <erfan.neshati@gmail.com>
2025-07-03 19:58:24 +0330

    fit(app.js): add color option for battery interface

--- js/app.js
+++ js/app.js
@@ -9,7 +9,27 @@ const showBatteryInfo = () => {
     if (resault.charging) {
       batteryStatus.innerHTML = `Charging... <i class="ri-flashlight-line animated-green"></i>`;
     }
+    if (resault.level) {
+      const percentage = resault.level * 100;
+      batteryPercentage.innerHTML = `${percentage}%`;
+      batteryLiquid.style.height = `${percentage}%`;
 
+      console.log(batteryLiquid.className);
+      
+
+      if (percentage <= 100) {
+        batteryLiquid.className = "battery__liquid gradient-color-green";
+      }
+      if (percentage <= 90) {
+        batteryLiquid.className = "battery__liquid gradient-color-yellow";
+      }
+      if (percentage <= 40) {
+        batteryLiquid.className = "battery__liquid gradient-color-orange";
+      }
+      if (percentage <= 20) {
+        batteryLiquid.className = "battery__liquid gradient-color-red";
+      }
+    }
   });
 };
 
