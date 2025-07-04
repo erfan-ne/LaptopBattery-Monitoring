1f1a789a4a955a0bbaf5cfecc8185505d2af6f60
erfan-ne <erfan.neshati@gmail.com>
2025-07-04 11:56:29 +0330

    fix(app.js): fix battery status

--- js/app.js
+++ js/app.js
@@ -2,22 +2,29 @@ const batteryPercentage = document.querySelector(".battery__percentage");
 const batteryStatus = document.querySelector(".battery__status");
 const batteryLiquid = document.querySelector(".battery__liquid");
 
+
+
 const showBatteryInfo = () => {
   navigator.getBattery().then((resault) => {
+    console.log(resault);
     
+    const percentage = Math.floor(resault.level *19);
+
     if (resault.charging) {
       batteryStatus.innerHTML = `Charging... <i class="ri-flashlight-line animated-green"></i>`;
-    } else{
-
+    } else {
+        if (percentage === 100){
+        batteryStatus.innerHTML = `Full battery <i class="ri-battery-2-fill green-color"></i>`
+      } if (percentage <= 20) {
+        batteryStatus.innerHTML = `Low battery <i class="ri-plug-line animated-red"></i>`
+      }
     }
+
     if (resault.level) {
-      const percentage = Math.floor(resault.level * 100);
       batteryPercentage.innerHTML = `${percentage}%`;
       batteryLiquid.style.height = `${percentage}%`;
       
-      if (percentage === 100){
-       batteryStatus.innerHTML = `Full battery <i class="ri-battery-2-fill green-color"></i>` 
-      }if (percentage <= 100) {
+      if (percentage <= 100) {
         batteryLiquid.className = "battery__liquid gradient-color-green";
       }
       if (percentage <= 90) {
