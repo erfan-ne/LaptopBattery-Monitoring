6004c0f42bd027233a35084e4813d7f0bd1b657a
erfan-ne <erfan.neshati@gmail.com>
2025-07-04 12:22:55 +0330

    fix(app.js): fix battery status

--- js/app.js
+++ js/app.js
@@ -2,28 +2,29 @@ const batteryPercentage = document.querySelector(".battery__percentage");
 const batteryStatus = document.querySelector(".battery__status");
 const batteryLiquid = document.querySelector(".battery__liquid");
 
-
-
 const showBatteryInfo = () => {
   navigator.getBattery().then((resault) => {
     console.log(resault);
-    
-    const percentage = Math.floor(resault.level *19);
 
-    if (resault.charging) {
+    const percentage = Math.floor(resault.level * 100);
+
+    if (resault.charging === true && percentage === 100) {
+      batteryStatus.innerHTML = `Full Battery <i class="ri-flashlight-line animated-green"></i>`;
+    } else if (resault.charging === true) {
       batteryStatus.innerHTML = `Charging... <i class="ri-flashlight-line animated-green"></i>`;
     } else {
-        if (percentage === 100){
-        batteryStatus.innerHTML = `Full battery <i class="ri-battery-2-fill green-color"></i>`
-      } if (percentage <= 20) {
-        batteryStatus.innerHTML = `Low battery <i class="ri-plug-line animated-red"></i>`
+      if (percentage === 100) {
+        batteryStatus.innerHTML = `Full battery <i class="ri-battery-2-fill green-color"></i>`;
+      }
+      if (percentage <= 20) {
+        batteryStatus.innerHTML = `Low battery <i class="ri-plug-line animated-red"></i>`;
       }
     }
 
     if (resault.level) {
       batteryPercentage.innerHTML = `${percentage}%`;
       batteryLiquid.style.height = `${percentage}%`;
-      
+
       if (percentage <= 100) {
         batteryLiquid.className = "battery__liquid gradient-color-green";
       }
@@ -35,12 +36,10 @@ const showBatteryInfo = () => {
       }
       if (percentage <= 20) {
         batteryLiquid.className = "battery__liquid gradient-color-red";
-        batteryStatus.innerHTML = `Low battery <i class="ri-plug-line animated-red"></i>`
       }
     }
 
-    resault.addEventListener("levelchange" , showBatteryInfo)
-
+    resault.addEventListener("levelchange", showBatteryInfo);
   });
 };
 
